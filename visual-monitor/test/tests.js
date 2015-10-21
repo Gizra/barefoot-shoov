aa8c893f8fdf87c1454dfb4afad08f8b1ec4ad83
DavidKohav <david@gizra.com>
2015-10-21 12:15:18 +0300

    Squashed #1 : Add a visual monitor.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -63,10 +63,16 @@ describe('Visual monitor testing', function() {
       .url(baseUrl)
       .webdrivercss(testName + '.homepage', {
         name: '1',
-        exclude: [],
-        remove: [],
-        hide: [],
-        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+        exclude:
+          [
+            '#hs_cos_wrapper_widget_3455565 a img'
+          ],
+        hide:
+          [
+            // Content text.
+            '.ISContent'
+          ],
+        screenWidth: selectedCaps == 'chrome' ? [960] : undefined,
       }, resultsCallback)
       .call(done);
   });
